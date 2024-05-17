38055b3c05b1305d80db1be192c5737f84e85ec6
Adedamola A. Adeagbo <154677867+Dedamola1@users.noreply.github.com>
2024-05-17 13:42:51 +0100

    Add files via upload

---
+++ Script-Importing.sql
@@ -0,0 +1,207 @@
+CREATE TABLE cheese_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+-- Import Data into Table Cheese_production
+
+CREATE TABLE cheese_production_new (
+	Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+INSERT INTO cheese_production_new 
+("Year","    Period","    Geo_Level","    State_ANSI","    Commodity_ID","    Domain","    Value")
+SELECT *
+FROM cheese_production;
+
+UPDATE cheese_production_new  SET "    Value"  = REPLACE("    Value" , ',', '');
+
+DROP TABLE cheese_production;
+
+ALTER TABLE cheese_production_n RENAME YO cheese_production;
+
+
+
+CREATE TABLE honey_production (
+    Year INTEGER,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+-- Import Data into Table honey_production
+
+CREATE TABLE honey_production_new (
+    Year INTEGER,
+	Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+INSERT INTO honey_production_new ("Year" , "Geo_level","State_ANSI","Commodity_ID" ,"Value")
+SELECT *
+FROM honey_production;
+
+UPDATE honey_production_new SET Value = REPLACE(Value, ',', '');
+
+DROP Table	honey_production;
+
+ALTER TABLE honey_production_new RENAME TO honey_production;
+
+
+
+
+CREATE TABLE milk_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+-- Import Data into Table milk_production
+
+CREATE TABLE milk_production_NEW(
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+INSERT INTO milk_production_NEW ("    Year" ,"    Period","    Geo_Level","    State_ANSI","    Commodity_ID","    Domain","    Value")
+SELECT *
+FROM milk_production ;
+
+UPDATE milk_production_NEW SET Value = REPLACE(Value, ',', '');
+
+DROP Table	milk_production ;
+
+ALTER TABLE milk_production_NEW RENAME TO milk_production;
+
+
+
+
+CREATE TABLE coffee_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+-- Import Data into Table coffee_production
+
+CREATE TABLE coffee_production_NEW (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+INSERT INTO coffee_production_NEW  ("    Year" ,"    Period","    Geo_Level","    State_ANSI","    Commodity_ID","    Value")
+SELECT *
+FROM coffee_production;
+
+UPDATE coffee_production_NEW  SET Value = REPLACE(Value, ',', '');
+
+DROP Table	coffee_production;
+
+ALTER TABLE coffee_production_NEW  RENAME TO coffee_production;
+
+
+
+
+CREATE TABLE egg_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+-- Import Data into Table milk_production
+
+CREATE TABLE egg_production_NEW (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+INSERT INTO egg_production_NEW  ("    Year" ,"    Period","    Geo_Level","    State_ANSI","    Commodity_ID","    Value")
+SELECT *
+FROM egg_production;
+
+UPDATE egg_production_NEW  SET Value = REPLACE(Value, ',', '');
+
+DROP Table	egg_production ;
+
+ALTER TABLE egg_production_NEW  RENAME TO egg_production;
+
+
+
+
+CREATE TABLE state_lookup (
+    State TEXT,
+    State_ANSI INTEGER
+);
+
+
+CREATE TABLE yogurt_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+-- Import Data into Table yogurt_production
+
+CREATE TABLE yogurt_production_NEW (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+INSERT INTO yogurt_production_NEW  ("    Year" ,"    Period","    Geo_Level","    State_ANSI","    Commodity_ID","    Domain" ,"    Value")
+SELECT *
+FROM yogurt_production;
+
+UPDATE yogurt_production_NEW  SET Value = REPLACE(Value, ',', '');
+
+DROP Table yogurt_production ;
+
+ALTER TABLE yogurt_production_NEW  RENAME TO yogurt_production;
+
+
+
---
+++ Script-Main.sql
@@ -0,0 +1,112 @@
+-- Total production value by year for Milk, Coffee, Honey, Yogurt, Egg and Cheese
+SELECT "Year" , SUM(Value) 
+FROM milk_production mp 
+GROUP BY "Year" 
+;
+
+SELECT "Year" , SUM(Value) 
+FROM coffee_production cp 
+GROUP BY "Year"
+;
+
+SELECT "Year" , SUM(Value) 
+FROM honey_production hp 
+GROUP BY "Year"
+;
+
+SELECT "Year", SUM(Value) 
+FROM yogurt_production yp
+GROUP BY "Year"
+; 
+
+SELECT "Year", SUM(Value) 
+FROM egg_production ep 
+GROUP BY "Year"
+; 
+
+SELECT "Year", SUM(Value) 
+FROM cheese_production cp 
+GROUP BY "Year"
+; 
+
+-- Total Milk Production for the year 2023
+SELECT SUM(Value) AS Total_Milk_Production
+FROM milk_production mp 
+WHERE "Year" = 2023;
+
+-- States that had cheese production greater than 100 million in April 2023
+SELECT cp.State_ANSI, 
+		sl.State,  
+		Value 
+FROM cheese_production cp
+LEFT JOIN state_lookup sl 
+			ON sl.State_ANSI = cp.State_ANSI 
+WHERE "Year" = 2023
+AND Period = 'APR'
+AND Value > 100000000;
+
+-- Total value of coffee production for 2011
+SELECT Value 
+FROM coffee_production cp 
+WHERE "Year" = 2011;
+
+-- Average honey production for 2022
+SELECT AVG(Value) AS Average_Honey_Production 
+FROM honey_production hp 
+WHERE "Year" = 2022;
+
+-- State_ANSI code for Florida
+SELECT *
+FROM state_lookup sl
+WHERE State = 'FLORIDA';
+
+-- States which produced Cheese in April 2023
+SELECT "Year" ,
+		Period , 
+		sl.State ,
+		Value 
+FROM cheese_production cp 
+LEFT JOIN state_lookup sl 
+			ON sl.State_ANSI = cp.State_ANSI 
+WHERE cp."Year" = 2023
+AND Period = 'APR'
+;
+
+--Total yogurt production for states in the year 2022 that also have cheese production data from 2023
+SELECT SUM(Value) AS Total_Yogurt_Production
+FROM yogurt_production yp 
+WHERE yp."Year" = 2022 
+AND yp.State_ANSI IN (SELECT DISTINCT cp.State_ANSI 
+						FROM cheese_production cp
+						WHERE "Year" = 2023
+);
+
+-- States that are missing from Milk Production in 2023
+SELECT COUNT(*) 
+FROM state_lookup sl 
+WHERE State_ANSI NOT IN (SELECT State_ANSI 
+						FROM milk_production mp 
+						WHERE "Year" =2023
+);
+
+-- Cheese production values and states in april 20234
+SELECT "Year" , 
+		Period, 
+		sl.State, 
+		cp.Value 
+FROM cheese_production cp 
+LEFT JOIN state_lookup sl 
+			ON sl.State_ANSI = cp.State_ANSI 
+WHERE "Year" = 2023
+AND Period = 'APR'
+;
+
+-- Average coffee production for all years where the honey production exceeded 1 million
+SELECT AVG(Value) AS Average_Coffee_Production 
+FROM coffee_production cp 
+WHERE "Year" IN (SELECT DISTINCT "Year" 
+					FROM honey_production hp 
+					WHERE Value > 1000000
+);
+
+
